eaaed56f18fae11f061f22c7da90c3ee5a262c1a
Criskanda <cris_kanda_93@hotmail.com>
2015-02-24 12:58:09 +0100

    rest

--- gen/reboot/vnote/R.java
+++ gen/reboot/vnote/R.java
@@ -26,13 +26,21 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int add=0x7f080005;
-        public static final int delete=0x7f080007;
-        public static final int edit=0x7f080006;
+        public static final int action_settings=0x7f080008;
+        public static final int add=0x7f08000a;
+        public static final int delete=0x7f08000c;
+        public static final int edit=0x7f08000b;
+        public static final int et_content=0x7f080007;
+        public static final int et_title=0x7f080004;
+        public static final int listen=0x7f08000f;
         public static final int lv_notes=0x7f080002;
         public static final int main_activity=0x7f080000;
-        public static final int search=0x7f080004;
+        public static final int save=0x7f08000d;
+        public static final int scrollView1=0x7f080003;
+        public static final int search=0x7f080009;
+        public static final int speach=0x7f08000e;
+        public static final int textView1=0x7f080005;
+        public static final int tv_content=0x7f080006;
         public static final int tv_notes=0x7f080001;
     }
     public static final class layout {
@@ -41,6 +49,7 @@ public final class R {
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int note=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -48,9 +57,16 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int delete=0x7f050004;
         public static final int edit=0x7f050005;
+        public static final int listen=0x7f05000e;
+        public static final int note_content=0x7f05000b;
+        public static final int note_title=0x7f050009;
         public static final int notes=0x7f050007;
+        public static final int save=0x7f05000c;
         public static final int search=0x7f050002;
+        public static final int speach=0x7f05000d;
         public static final int title_activity_new_note=0x7f050006;
+        public static final int tv_content=0x7f05000a;
+        public static final int tv_tittle=0x7f050008;
     }
     public static final class style {
         /** 
