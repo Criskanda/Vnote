3bf1135e14737a348f36d36fd261e1665fb54cf1
Criskanda <cris_kanda_93@hotmail.com>
2015-02-24 15:56:27 +0100

    rest

--- gen/reboot/vnote/R.java
+++ gen/reboot/vnote/R.java
@@ -30,10 +30,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
-        public static final int add=0x7f09000a;
-        public static final int delete=0x7f09000c;
-        public static final int edit=0x7f09000b;
+        public static final int action_settings=0x7f09000c;
+        public static final int add=0x7f090009;
+        public static final int delete=0x7f09000b;
+        public static final int edit=0x7f09000a;
         public static final int et_content=0x7f090007;
         public static final int et_title=0x7f090004;
         public static final int listen=0x7f09000f;
@@ -41,7 +41,7 @@ public final class R {
         public static final int main_activity=0x7f090000;
         public static final int save=0x7f09000d;
         public static final int scrollView1=0x7f090003;
-        public static final int search=0x7f090009;
+        public static final int search=0x7f090008;
         public static final int speach=0x7f09000e;
         public static final int textView1=0x7f090005;
         public static final int tv_content=0x7f090006;
