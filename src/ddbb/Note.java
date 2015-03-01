ca915ebaeac52abec8ef86fcd7caf8ee428a0d96
Criskanda <cristiancvacas@gmail.com>
2015-03-01 17:02:48 +0100

    Added new field "date"

--- src/ddbb/Note.java
+++ src/ddbb/Note.java
@@ -1,11 +1,15 @@
 package ddbb;
 
+import java.sql.Date;
+
 public class Note {
 
 	private int id;
     private String title, content;
+    private Date date;
     
-    public Note(int id,String title, String content){
+ 
+	public Note(int id,String title, String content){
     	super();
     	this.id = id;
         this.title = title;
@@ -40,7 +44,14 @@ public class Note {
     public void setContent(String content){
         this.content = content;
     }
-    
+    public Date getDate() {
+ 		return date;
+ 	}
+
+ 	public void setDate(Date date) {
+ 		this.date = date;
+ 	}
+
     @Override
     public String toString(){
     	return this.title;
