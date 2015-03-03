597c75bdb8f952b5147c2661e60e7926807da3ab
Criskanda <cris_kanda_93@hotmail.com>
2015-03-03 12:39:44 +0100

    rest

--- gen/reboot/vnote/R.java
+++ gen/reboot/vnote/R.java
@@ -30,19 +30,20 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000c;
-        public static final int add=0x7f090009;
-        public static final int delete=0x7f09000b;
-        public static final int edit=0x7f09000a;
+        public static final int action_settings=0x7f09000e;
+        public static final int add=0x7f09000c;
+        public static final int delete=0x7f090009;
+        public static final int edit=0x7f09000d;
         public static final int et_content=0x7f090007;
         public static final int et_title=0x7f090004;
-        public static final int listen=0x7f09000f;
+        public static final int listen=0x7f09000a;
         public static final int lv_notes=0x7f090002;
         public static final int main_activity=0x7f090000;
-        public static final int save=0x7f09000d;
+        public static final int save=0x7f09000f;
         public static final int scrollView1=0x7f090003;
-        public static final int search=0x7f090008;
-        public static final int speach=0x7f09000e;
+        public static final int search=0x7f09000b;
+        public static final int send=0x7f090008;
+        public static final int speach=0x7f090010;
         public static final int textView1=0x7f090005;
         public static final int tv_content=0x7f090006;
         public static final int tv_notes=0x7f090001;
@@ -50,10 +51,13 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_note=0x7f030001;
+        public static final int activity_selectitems=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int note=0x7f080001;
+        public static final int context_main=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int note=0x7f080002;
+        public static final int select=0x7f080003;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
@@ -67,6 +71,7 @@ public final class R {
         public static final int notes=0x7f060007;
         public static final int save=0x7f06000c;
         public static final int search=0x7f060002;
+        public static final int send=0x7f06000f;
         public static final int speach=0x7f06000d;
         public static final int title_activity_new_note=0x7f060006;
         public static final int tv_content=0x7f06000a;
