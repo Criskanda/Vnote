0881c520250aa22f204dac0ce600d467d21dd308
Criskanda <cristiancvacas@gmail.com>
2015-03-04 23:05:50 +0100

    fixed bug with title

--- src/ddbb/Note.java
+++ src/ddbb/Note.java
@@ -1,76 +1,55 @@
 package ddbb;
 
 public class Note {
+	 
+ 	private int id;
+    private String title, content,date;
+    
+ 
+	public Note(int id,String title, String content){
+    	super();
+    	this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+    
+    public Note(String title){
+    	super();
+        this.title = title;
+    }
+    
+    public int getId(){
+        return this.id;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    // setting title
+    public void setTitle(String title){
+        this.title = title;
+    }
+    // getting content
+    public String getContent(){
+        return this.content;
+    }
+    //setting content
+    public void setContent(String content){
+        this.content = content;
+    }
+    public String getDate() {
+ 		return date;
+ 	}
 
-	private int id;
-	private String title, content, date;
+ 	public void setDate(String date) {
+ 		this.date = date;
+ 	}
 
-	public Note(int id, String title, String content) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.content = content;
-	}
-
-	public Note(String title) {
-		super();
-		this.title = title;
-	}
-
-	/**
-	 * @return the id
-	 */
-	public int getId() {
-		return id;
-	}
-
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the title
-	 */
-	public String getTitle() {
-		return title;
-	}
-
-	/**
-	 * @param title the title to set
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	/**
-	 * @return the content
-	 */
-	public String getContent() {
-		return content;
-	}
-
-	/**
-	 * @param content the content to set
-	 */
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	/**
-	 * @return the date
-	 */
-	public String getDate() {
-		return date;
-	}
-
-	/**
-	 * @param date the date to set
-	 */
-	public void setDate(String date) {
-		this.date = date;
-	}
-
+    @Override
+    public String toString(){
+    	return this.title;
+    }
 }
