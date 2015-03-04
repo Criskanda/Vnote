e75dcf280d59d94d9b5ab2105e3d0a424e5b46d8
Criskanda <cristiancvacas@gmail.com>
2015-03-04 22:14:21 +0100

    Commented "Conexion" class

--- src/ddbb/Conexion.java
+++ src/ddbb/Conexion.java
@@ -13,6 +13,7 @@ import android.database.sqlite.SQLiteStatement;
 public class Conexion extends SQLiteOpenHelper{
 	
 	String sql ="CREATE TABLE notes (id INT PRIMARYKEY AUTO INCREMENT, title TEXT, content TEXT, date DATE)";
+	
 	public Conexion(Context context, String name, CursorFactory factory,
 			int version) {
 		super(context, name, factory, version);
@@ -28,20 +29,34 @@ public class Conexion extends SQLiteOpenHelper{
 		
 	}
 	
+	/**
+	 * 
+	 * @param db SQLitedatabase
+	 * @param title, title of the note
+	 * @param content, content of the note, no max.
+	 * @param date, date of the create.
+	 */
 	public void InsertNote (SQLiteDatabase db, String title, String content, String date){
 	SQLiteStatement pst = db.compileStatement("INSERT INTO notes (title, content,date) VALUES (?,?,?)");
 	pst.bindString(1, title);
 	pst.bindString(2, content);
 	pst.bindString(3, date);
-
 	pst.execute();
 	}
 	
+	/**
+	 * 
+	 * @param title for search in the ddbb in where clause
+	 */
 	public void deleteNote(String title) {
 	    SQLiteDatabase db = getWritableDatabase();
 	    db.delete("notes", "title='"+title+"'", null);
 	    db.close();  
 	}
+	/**
+	 * 
+	 * @return day, month, year and exact time
+	 */
 	public String getToday(){
 		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		Date date = new Date();
